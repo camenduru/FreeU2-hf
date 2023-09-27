64efbd0de4d6d942cf024e7f8723a2b9cd2ba606
ChenyangSi <ChenyangSi@users.noreply.huggingface.co>
2023-09-27 15:46:41 +0000

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from diffusers import StableDiffusionPipeline
 import torch
 from free_lunch_utils import register_free_upblock2d, register_free_crossattn_upblock2d
 
+torch.manual_seed(42)
 
 model_id = "CompVis/stable-diffusion-v1-4"
         
@@ -20,10 +21,10 @@ pip_sd = pip_sd.to("cuda")
 
 pip_freeu = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
 pip_freeu = pip_freeu.to("cuda")
-# # -------- freeu block registration
-# register_free_upblock2d(pip_freeu, b1=1.2, b2=1.4, s1=0.9, s2=0.2)
-# register_free_crossattn_upblock2d(pip_freeu, b1=1.2, b2=1.4, s1=0.9, s2=0.2)
-# # -------- freeu block registration
+# -------- freeu block registration
+register_free_upblock2d(pip_freeu, b1=1.2, b2=1.4, s1=0.9, s2=0.2)
+register_free_crossattn_upblock2d(pip_freeu, b1=1.2, b2=1.4, s1=0.9, s2=0.2)
+# -------- freeu block registration
 
 def infer(prompt):
     
