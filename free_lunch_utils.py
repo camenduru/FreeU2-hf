98d6951cb628e2f8e062ee3f27b7cc5fd0c99299
ChenyangSi <ChenyangSi@users.noreply.huggingface.co>
2023-09-27 16:36:21 +0000

    Update free_lunch_utils.py

--- free_lunch_utils.py
+++ free_lunch_utils.py
@@ -20,26 +20,6 @@ def isinstance_str(x: object, cls_name: str):
     return False
 
 
-def Fourier_filter(x, threshold, scale):
-    dtype = x.dtype
-    x = x.type(torch.float32)
-    # FFT
-    x_freq = fft.fftn(x, dim=(-2, -1))
-    x_freq = fft.fftshift(x_freq, dim=(-2, -1))
-    
-    B, C, H, W = x_freq.shape
-    mask = torch.ones((B, C, H, W)).cuda() 
-
-    crow, ccol = H // 2, W //2
-    mask[..., crow - threshold:crow + threshold, ccol - threshold:ccol + threshold] = scale
-    x_freq = x_freq * mask
-
-    # IFFT
-    x_freq = fft.ifftshift(x_freq, dim=(-2, -1))
-    x_filtered = fft.ifftn(x_freq, dim=(-2, -1)).real
-    
-    x_filtered = x_filtered.type(dtype)
-    return x_filtered
 
 
 def register_upblock2d(model):
