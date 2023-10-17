35ef96b7083b64cb3ad24926d607a6979130b89e
ChenyangSi <ChenyangSi@users.noreply.huggingface.co>
2023-10-17 02:12:45 +0000

    Update app.py

--- app.py
+++ app.py
@@ -159,12 +159,12 @@ with block:
                 with gr.Row():
                     b1 = gr.Slider(label='b1: backbone factor of the first stage block of decoder',
                                             minimum=1,
-                                            maximum=1.6,
+                                            maximum=2.0,
                                             step=0.01,
                                             value=1.6)
                     b2 = gr.Slider(label='b2: backbone factor of the second stage block of decoder',
                                             minimum=1,
-                                            maximum=1.6,
+                                            maximum=2.0,
                                             step=0.01,
                                             value=1.6)
                 with gr.Row():
